908879d1efd99574b92c7bbe1bf3391c5006d1fa
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2022-03-22 18:55:13 +0800

    Create TreeTestUI.java

---
+++ cn/imaginary/ui/TreeTestUI.java
@@ -0,0 +1,17 @@
+package cn.imaginary.ui;
+
+import javax.swing.JFrame;
+
+public class TreeTestUI extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5728753561881129921L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
