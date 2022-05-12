b9e76dd216ec293a3f6f4975bcdbbb2b4ffaf0bc
Sev末夜 <52105884+neverever1533@users.noreply.github.com>
2022-05-12 17:13:53 +0800

    Update PSDFileReader.java

--- src/cn/imaginary/toolkit/io/PSDFileReader.java
+++ src/cn/imaginary/toolkit/io/PSDFileReader.java
@@ -5,7 +5,9 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 import cn.imaginary.toolkit.image.ImageLayer;
@@ -20,6 +22,21 @@ import psd.model.Psd;
 public class PSDFileReader {
 	private FileUtils fileUtils = FileUtils.getInstance();
 
+	/*public static void main(String[] args) {
+		PSDFileReader pfr = new PSDFileReader();
+		ArrayList<ImageLayer> aList = new ArrayList<>();
+		String fp = "e:\\Downloads\\Models\\Evolution Vector Model\\未标题-1.psd";
+		File f = new File(fp);
+		pfr.read(f, aList);
+		ImageLayer layer;
+		for (Iterator<ImageLayer> iterator = aList.iterator(); iterator.hasNext();) {
+			layer = (ImageLayer) iterator.next();
+			if (null != layer) {
+				System.out.println(layer.getProperties());
+			}
+		}
+	}*/
+
 	public List<ImageLayer> read(File file, List<ImageLayer> layerList) {
 		if (null != file && null != layerList) {
 			try {
@@ -27,7 +44,6 @@ public class PSDFileReader {
 				int lc = p.getLayersCount();
 				Layer layer;
 				BufferedImage image;
-				int index = 0;
 				File fileTemp;
 				float alpha;
 				int x;
@@ -39,15 +55,15 @@ public class PSDFileReader {
 						image = layer.getImage();
 						x = layer.getX();
 						y = layer.getY();
-//						System.out.println(x);
-//						System.out.println(y);
 						alpha = layer.getAlpha() / 255;
-						fileTemp = fileUtils.getFile(file, index++, ImageIOUtils.FileSuffixes_Default);
+						fileTemp = fileUtils.getFile(file, i, ImageIOUtils.FileSuffixes_Default);
 						imageLayer = new ImageLayer();
 						imageLayer.setImage(image);
 						imageLayer.setImagePath(fileTemp.getPath());
-						imageLayer.setLocation(x, y);
 						imageLayer.setAlpha(alpha);
+						imageLayer.setDepth(i);
+						imageLayer.setLocation(x, y);
+						imageLayer.setRotated(0);
 						layerList.add(imageLayer);
 					}
 				}
